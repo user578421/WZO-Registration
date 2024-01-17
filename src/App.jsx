a4c66ac2605dcc275e1edcb66a301220a9c0df68
Akiva Kraines <akraines@gmail.com>
2024-01-17 16:29:08 +0200

    Add instructions

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import jsPDF from "jspdf";
 import {y} from './david1.js'
 import {x} from './david2.js'
 import {useAtom} from "jotai";
-import {isFormFilledAtom, isProcessingAtom} from "./atoms.js";
+import {familyNameAtom, isFormFilledAtom, isProcessingAtom, nameAtom} from "./atoms.js";
 import {ToastContainer} from "react-toastify";
 
 import 'react-toastify/dist/ReactToastify.css';
@@ -15,9 +15,20 @@ function App() {
     const templateRef = useRef();
     const [isFormFilled] = useAtom(isFormFilledAtom);
     const [, setIsProcessingAtom] = useAtom(isProcessingAtom);
+    const [name]=useAtom(nameAtom);
+    const [family]=useAtom(familyNameAtom);
     console.log({isFormFilled})
+    //Todo: Add nicely formatted instructions:
     return (
         <div className="min-h-screen bg-gray-100 p-0 sm:p-12 flex justify-center flex-col items-center">
+            <div className={"mb-5"}>
+                Instructions:
+                <ul className={"list-disc list-inside"}>
+                    <li>Fill out the form below</li>
+                    <li>Download the PDF</li>
+                    <li>Click on the button to open an email to the WZO Office and attach the PDF</li>
+                </ul>                 
+            </div>
             <InputForm/>
             <PdfTemplate templateRef={templateRef}/>
             <Button
@@ -44,13 +55,24 @@ function App() {
             >
                 Download PDF
             </Button>
+           
+            <SubTitle>
+                The email should be sent to the following addresses:
+                <ul className={"list-disc list-inside"}>
+                    <li>To: {["yaakovH!wzo.org.il", "yaakova!wzo.org.il", "gustiY!wzo.org.il", "reubensh!wzo.org.il"].join(";").replace(/!/g, "@")}</li>
+                    <li>BCC: {"wzoelections!gmail.com".replace(/!/g, "@")}</li>
+                    <li>Subject: WZO Registration Form - {name} {family}</li>
+                    <li>Body: Please find attached the WZO Registration Form</li>
+                    <li>Attach the PDF</li>
+                </ul>
+            </SubTitle>
             <Button
                 disabled={!isFormFilled}
                 onClick={() => {
                     //dont store the email addresses in the code so that they are not exposed in the public source code
                     const toAddresses = ["yaakovH!wzo.org.il", "yaakova!wzo.org.il", "gustiY!wzo.org.il", "reubensh!wzo.org.il"].join(",").replace(/!/g, "@");
-                    const bccAddresses = "wzoelections!gmail.com".replace(/!/g, "@"); 
-                    const subject = "WZO Registration Form";
+                    const bccAddresses = "wzoelections!gmail.com".replace(/!/g, "@");
+                    const subject = `WZO Registration Form - ${name} ${family}`;
                     const body = "Please find attached the WZO Registration Form";
                     const href = `mailto:${toAddresses}?subject=${subject}&body=${body}&bcc=${bccAddresses}`;
                     //create a link and click it
